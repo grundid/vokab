aa5f94b52fcaf3c99a6a9c7b31eb09db8f6f806b
Adrian Stabiszewski <as@grundid.de>
2017-11-06 13:33:27 +0100

    remove line breaks

--- index.js
+++ index.js
@@ -1,40 +1,27 @@
-var srt = require("srt")
-
+const srt = require("srt");
 
 const subtitles = [];
+
+function sortAndOutput() {
+    subtitles.sort(function (a, b) {
+        return a.length - b.length;
+    });
+
+
+    subtitles.forEach(function (e) {
+       console.log("["+e+"]");
+    });
+}
 
 srt("data/englisch/Game.of.Thrones.S01E01.srt", function (err, data) {
     if (err) {
         console.log(err);
     } else {
-        console.log("enter callback");
-
         Object.keys(data).forEach((key) => {
-            const subtitle = data[key].text;
+            const subtitle = data[key].text.replace(/\r|\n/g, ' ');
             subtitles.push(subtitle);
         });
-        console.log("exit callback");
-
-
-        console.log("after srt");
-
-        subtitles.sort(function (a, b) {
-            return a.length - b.length;
-        });
-
-
-        console.log("after sort");
-
-        subtitles.forEach(function (e) {
-            // console.log("["+e+"]");
-        });
+        sortAndOutput();
     }
 });
-
-srt("asdasd").then(function (data) {
-
-}).catch(function (err) {
-
-});
-
 
