4eb1be99da9bf47b1f8bc931933a8d00feda53c7
arman torkzaban <arman@arman.codenameact.org>
2017-11-13 14:09:08 +0100

    what

---
+++ addressKey.js
@@ -0,0 +1,22 @@
+const array1 = [{
+    word: "hello",
+    number: 1
+}, {
+    word: "Bye",
+    number: 2
+}];
+const arraty2 = [{
+    word: "hallo",
+    number: 1
+}, {
+    word: "Bye",
+    number: 2
+}];
+
+
+const results = {
+    uniqueInA: { hello: 1 },
+    uniqueInB: { hallo: 1 },
+    common: { Bye: 4 }
+};
+
